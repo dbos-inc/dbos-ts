fc932fabf2d3c3954f72cf575dc077d7475e355c
Harry Pierson <harry.pierson@dbos.dev>
2024-04-09 17:41:05 -0700

    cleanup PGNodeUserDatabase.transaction

--- src/user_database.ts
+++ src/user_database.ts
@@ -73,20 +73,21 @@ export class PGNodeUserDatabase implements UserDatabase {
   async transaction<R, T extends unknown[]>(txn: UserDatabaseTransaction<R, T>, config: TransactionConfig, ...args: T): Promise<R> {
     const client: PoolClient = await this.pool.connect();
     try {
+      if (config.storedProc) {
+        // if stored proc is specified, assume it handles transaction management
+        return txn(client, ...args);
+      }
       const readOnly = config.readOnly ?? false;
       const isolationLevel = config.isolationLevel ?? IsolationLevel.Serializable;
-
-      // if stored proc is specified, assume it handles transaction management
-      if (!config.storedProc) {
-        await client.query(`BEGIN ISOLATION LEVEL ${isolationLevel}`);
+      await client.query(`BEGIN ISOLATION LEVEL ${isolationLevel}`);
+      if (readOnly) {
         // TODO: this should be combined with previous statement to save a DB round trip
         //       PG simple queries support multiple statements separated by semicolon
         //       as long as they don't have parameters
-        if (readOnly) { await client.query(`SET TRANSACTION READ ONLY`); }
+        await client.query(`SET TRANSACTION READ ONLY`);
       }
       const result: R = await txn(client, ...args);
-      // see above regarding tx management in stored proc cases
-      if (!config.storedProc) { await client.query(`COMMIT`); }
+      await client.query(`COMMIT`);
       return result;
     } catch (err) {
       // see above regarding tx management in stored proc cases
